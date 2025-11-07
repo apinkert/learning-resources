7e6409415aa955e13ef75a9978040a90726174b2
Adam Jetmar <ajetmar@seznam.cz>
2025-11-07 16:27:49 +0100

    refactor(creator): Simplify YAML editor with @monaco-editor/react

--- src/components/creator/CreatorYAMLView.tsx
+++ src/components/creator/CreatorYAMLView.tsx
@@ -1,61 +1,31 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { PageSection } from '@patternfly/react-core';
-import * as monaco from 'monaco-editor';
+import Editor from '@monaco-editor/react';
 import './CreatorYAMLView.scss';
 
-// Disable Monaco workers globally
-self.MonacoEnvironment = {
-  getWorker() {
-    // Return a minimal worker that does nothing
-    return new Worker(
-      URL.createObjectURL(
-        new Blob(['self.onmessage = () => {}'], { type: 'text/javascript' })
-      )
-    );
-  },
-};
-
 const CreatorYAMLView: React.FC = () => {
-  const editorRef = useRef<HTMLDivElement>(null);
-  const editorInstanceRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(
-    null
-  );
-
-  useEffect(() => {
-    if (!editorRef.current) {
-      return;
-    }
-
-    const editor = monaco.editor.create(editorRef.current, {
-      value:
-        '# YAML Quickstart Definition\n# Start typing or paste your YAML here\n',
-      language: 'yaml',
-      theme: 'vs',
-      automaticLayout: true,
-      minimap: {
-        enabled: true,
-      },
-      scrollBeyondLastLine: false,
-      wordWrap: 'on',
-      fontSize: 14,
-      lineNumbers: 'on',
-      folding: true,
-      renderWhitespace: 'selection',
-    });
-
-    editorInstanceRef.current = editor;
-
-    return () => {
-      if (editorInstanceRef.current) {
-        editorInstanceRef.current.dispose();
-        editorInstanceRef.current = null;
-      }
-    };
-  }, []);
-
   return (
     <PageSection className="lr-c-creator-yaml-view">
-      <div ref={editorRef} className="lr-c-creator-yaml-view__editor" />
+      <div className="lr-c-creator-yaml-view__editor">
+        <Editor
+          height="100%"
+          language="yaml"
+          theme="vs"
+          defaultValue={
+            '# YAML Quickstart Definition\n# Start typing or paste your YAML here\n'
+          }
+          options={{
+            automaticLayout: true,
+            minimap: { enabled: true },
+            scrollBeyondLastLine: false,
+            wordWrap: 'on',
+            fontSize: 14,
+            lineNumbers: 'on',
+            folding: true,
+            renderWhitespace: 'selection',
+          }}
+        />
+      </div>
     </PageSection>
   );
 };
